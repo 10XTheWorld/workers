470dbf96995cd81fd22b96d3b8137b5918e2e8b5
Chris Jacob <i.chris.jacob@gmail.com>
2022-04-03 06:19:45 +1000

    Content template
    Comments for DRAFTs
    Comments for explaining fields 
    Filled in some examples of my DRAFTs
    utm_content = automatically filled in with SLUG (Content ID)

--- index.js
+++ index.js
@@ -1,39 +1,59 @@
 import { Router } from 'itty-router'
 
 // Content ID URL parameters for analytics
+//
+// FIELDS
+// 'utm_source'   = Content platform (e.g. youtube, twitter, giphy, medium)
+// 'utm_medium'   = Content type (e.g. video, tweet, gif, post)
+// 'utm_campaign' = Content strategy group (e.g. workout, exercise_gif, music, thread, summary, product_of_all_time)
+// 'utm_term'     = Content title (e.g. '10X Workout #5 Pushup, Crunch & Squat')
+// 'utm_id'       = Ads campaign id. (e.g. 'organic' if content is not tied to an ads campaign id)
+// 'utm_content'  = Content ID [PLEASE IGNORE. THIS IS AUTOMATICALLY ADDED] (e.g. '1', '2', '3'... same as the content 'key')
+//
+// DRAFT CONTENT IDs
+// Claim your future Content IDs early so other contributors know to not use these IDs
+// 3 = @chrisjacob 10X Music - Stay - Male
+// 2 = @chrisjacob 10X Music - Stay - Female
+// 1 = @chrisjacob Medium post on how to contribute 10X Content
+// 0 = @chrisjacob Easter Egg
+//
+// PUBLISHED CONTENT IDs
 const content = {
-  '1': {
-    'content_id': '1',
-    'foo': 'bar with spaces',
-    'utm': 'another'
-  },
-  '2': {
-    'content_id': '2',
-    'utm_id': 'abc.123456789',
-    'utm_source': 'google',
-    'utm_medium': 'cpc',
-    'utm_campaign': 'spring_sale',
-    'utm_term': 'running shoes',
-    'utm_content': 'logolink'
-  },
-  '3': {
-    'content_id': '3',
-    'utm_id': 'abc.123',
-    'utm_source': 'google',
-    'utm_medium': 'cpc',
-    'utm_campaign': 'spring_sale',
-    'utm_term': 'running shoes',
-    'utm_content': 'logolink'
-  },
   '4': {
-    'content_id': '4',
-    'utm_id': 'abc.123',
-    'utm_source': 'google',
-    'utm_medium': 'cpc',
-    'utm_campaign': 'spring_sale',
-    'utm_term': 'running shoes',
-    'utm_content': 'logolink'
+    'utm_source': 'youtube',
+    'utm_medium': 'video',
+    'utm_campaign': 'music',
+    'utm_term': 'TITLE (Song Lyrics) - ARTIST - 10X Men Workout Music',
+    'utm_id': 'organic'
   }
+//   '3': {
+//     'utm_source': 'youtube',
+//     'utm_medium': 'video',
+//     'utm_campaign': 'music',
+//     'utm_term': 'STAY (Song Lyrics) - The Kid LAROI, Justin Bieber - 10X Men Workout Music',
+//     'utm_id': 'organic'
+//   },
+//   '2': {
+//     'utm_source': 'youtube',
+//     'utm_medium': 'video',
+//     'utm_campaign': 'music',
+//     'utm_term': 'STAY (Song Lyrics) - The Kid LAROI, Justin Bieber - 10X Women Workout Music',
+//     'utm_id': 'organic'
+//   },
+//   '1': {
+//     'utm_source': 'medium',
+//     'utm_medium': 'post',
+//     'utm_campaign': 'content_machine',
+//     'utm_term': '10X Content Machine - Referrals On Autopilot',
+//     'utm_id': 'organic'
+//   },
+//   '0': {
+//     'utm_source': 'direct',
+//     'utm_medium': 'post',
+//     'utm_campaign': 'egg',
+//     'utm_term': '10X Content ID Easter Egg',
+//     'utm_id': 'organic'
+//   }
 }
 
 // Redirect to landingpage URL
@@ -56,9 +76,6 @@ Extract the short Content ID from URL then redirect to a landing page
 Try visit /1 and see the response.
 */
 router.get("/:slug", ({ params }) => {
-  // Decode text like "Hello%20world" into "Hello world"
-  // let input = decodeURIComponent(params.slug)
-  
   let content_id = params.slug;
   let content_data = content[content_id];
   
@@ -71,7 +88,7 @@ router.get("/:slug", ({ params }) => {
       .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(content_data[key])}`)
       .join('&');
         
-    let link = `${redirect_to}${content_id}?${query_string}`;
+    let link = `${redirect_to}${content_id}?utm_content={$content_id}&${query_string}`;
     
     return new Response(null, {
       headers: { Location: link },
